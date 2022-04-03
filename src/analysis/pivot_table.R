6ada404b9aa0b0e1f6c69b6f1175c31b0f0c452e
Marijekok <m.t.m.kok@tilburguniverstity.edu>
2022-04-03 10:21:21 +0200

    hallo

---
+++ src/analysis/pivot_table.R
@@ -0,0 +1,11 @@
+######################
+# CREATE PIVOT TABLE #
+######################
+
+# Load data
+df_grouped <- read.csv("../../gen/temp/df_grouped.csv")
+
+# create pivot table
+df_pivot <- df_grouped %>% dcast(date ~ neighbourhood, fun.aggregate = sum, value.var = "num_reviews")
+
+write.csv(df_pivot, "../../gen/temp/df_pivot.csv", row.names = FALSE)
